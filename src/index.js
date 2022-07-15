82cf5401a9b4279d99ae278c1600038999f8c79c
FilMaxim <fil.maxim13@gmail.com>
2022-07-15 22:42:57 +0300

    init:create towelsort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let result = '';
+  if (!Array.isArray(matrix) || !(matrix.length)) {
+    return [];
+  }
+   for (let i = 0; i < matrix.length; i++) {
+      if (i%2 === 0 || i === 0) {
+    result = result + matrix[i].join(',') + ','
+     } else {
+       result = result + matrix[i].reverse().join(',') + ','
+    }
+  }
+   result = result.slice(0, result.length - 1).split(',');
+   let num = result.map((elem) => Number(elem))
+   return num;
+ }
